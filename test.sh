cdd4a46d6fb95d57e4107c7f55d7675d5e89631e
Jarno Elovirta <jarno@elovirta.com>
2016-06-23 21:21:37 +0300

    Update and fix test script

--- test.sh
+++ test.sh
@@ -2,10 +2,10 @@
 
 function gen {
   ./get.py -o test/dita-ot-$1/plugins node pdf2 $1
-  ant -f test/dita-ot-$1/integrator.xml
+  ant -lib test/dita-ot-$1/lib -f test/dita-ot-$1/integrator.xml
   test/dita-ot-$1/bin/dita -i /Volumes/tmp/test.ditamap -f x -o test/dita-ot-$1/out -v
 }
 
-gen 2.1
 gen 2.2
 gen 2.3
+gen 3.0
