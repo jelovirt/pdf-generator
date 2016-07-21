36524a9ae45370ac3ef2fcdd5816aa3a58b3cff0
Jarno Elovirta <jarno@elovirta.com>
2016-07-21 15:42:49 +0300

    Clean and move functions to utils

--- generator.js
+++ generator.js
@@ -170,20 +170,6 @@ class Generator {
     return d.write()
   }
 
-  copy_xml(root, raw) {
-    const r = ET.parse(`
-<xsl:stylesheet xmlns:fo="http://www.w3.org/1999/XSL/Format"
-                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-                xmlns:e="e"
-                xmlns:opentopic="http://www.idiominc.com/opentopic"
-                xmlns:opentopic-func="http://www.idiominc.com/opentopic/exsl/function"
-                exclude-result-prefixes="e opentopic opentopic-func"
-                version="2.0">${raw}</xsl:stylesheet>`).getroot()
-    for (let i = 0; i < r.len(); i++) {
-      root.append(r.getItem(i))
-    }
-  }
-
   /**
    * Generate plugin custom XSLT file.
    */
@@ -1568,7 +1554,13 @@ class Generator {
 
     // custom XSLT
     if (this.override_shell) {
-      ["front-matter", "commons", "tables", "toc", "links", "lists", "static-content"].forEach((s) => {
+      ["front-matter",
+        "commons",
+        "tables",
+        "toc",
+        "links",
+        "lists",
+        "static-content"].forEach((s) => {
         this.run_generation(zip, () => {
           return this.generate_custom(s)
         }, `${this.plugin_name}/xsl/fo/${s}.xsl`)
@@ -1627,13 +1619,8 @@ function value(property, value) {
   return value
 }
 
-function xsl(elem) {
-  return '{http://www.w3.org/1999/XSL/Transform}' + elem
-}
-
-function catalog(elem) {
-  return '{urn:oasis:names:tc:entity:xmlns:xml:catalog}' + elem
-}
+const xsl = utils.xsl
+const catalog = utils.catalog
 
 module.exports = {
   Generator: Generator,
