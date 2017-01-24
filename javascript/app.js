7d9bc0d5dd8f07c6b573da4774e5023b21fa2e29
Jarno Elovirta <jarno@elovirta.com>
2017-01-24 17:32:49 +0200

    Hide patron feature from non-patron users

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,9 @@
+import $ from 'jquery'
 import PdfPageController from './app/PdfPageController'
 
 window.addEventListener('load', () => {
   PdfPageController()
+  // FIXME
+  $('.patron').remove()
+
 }, false)
